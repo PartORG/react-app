1a6c3761ff7932fb92e4afe22287629d93c6418c
Ievgen Perederieiev <peredereev.ievgen@gmail.com>
2022-12-30 12:17:31 +0100

    fix: use getOctikit.rest - and list of assignees fixed.

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -15,7 +15,7 @@ try {
         ...github.context.repo,
         title,
         body,
-        assignees: assignees ? assignees.split('\n') : undefined
+        assignees: [assignees]
     });
 
     core.setOutput('issue', JSON.stringify(response.data));
