9b414215df19748f97c6c049e34560af0294e54c
Ievgen Perederieiev <peredereev.ievgen@gmail.com>
2022-12-30 11:15:49 +0100

    feat: added more functionality from core JS package to demonstrate it's functionality more.

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -9688,14 +9688,24 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
 try {
-    throw( new Error("some error message"));
+    // throw( new Error("some error message"));
+
+    core.debug("Debug message - when debug mode is enabled in actions settings");
+    core.warning("Warning message.");
+    core.error("Error message.");
+
     const name = core.getInput("who-to-greet");
+    core.setSecret(name);  // will mask passed value - as secret - will not appear in logs
     console.log(`Hello ${name}`);
 
     const time = new Date();
     core.setOutput("time", time.toTimeString());
 
+    core.startGroup("Logging GitHub Object");  // create a group in logs that folds and unfolds
     console.log(JSON.stringify(github, null, "\t"));
+    core.endGroup();
+
+    core.exportVariable("Hello", "hello");
 } catch(error) {
     core.setFailed(error.message);
 } 
